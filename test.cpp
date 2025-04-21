ca91001c8755ce8571ad189e6c36d4cea6b9ab93
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-04-21 15:56:11 +0500

    мост

--- test.cpp
+++ test.cpp
@@ -4,3 +4,44 @@ LegacyCircle без Draw(), есть OldDrawCircle()
 CircleAdapter
 клиентский код
 */
+
+#include <iostream>
+using namespace std;
+
+class IShape {
+public:
+	virtual void Draw() const = 0;
+	virtual ~IShape() {}
+};
+
+class LegacyCircle {
+public:
+	void OldDrawCircle() const {    
+        cout << "Legacy Circle Drawing!" << endl;
+    }
+};
+
+class CircleAdapter : public IShape {
+private:
+    LegacyCircle* legacyCircle_;
+
+public:
+    CircleAdapter(LegacyCircle* legacyCircle) : legacyCircle_(legacyCircle) {}
+    void Draw() const override {
+        cout << "Circle Drawing!" << endl;
+        legacyCircle_->OldDrawCircle();
+    }
+};
+
+void ClientCode(const IShape* shape) {
+    shape->Draw();
+}
+
+int main() {
+    LegacyCircle* legacy = new LegacyCircle();
+    IShape* shape = new CircleAdapter(legacy);
+    ClientCode(shape);
+    delete shape;
+    delete legacy;
+    return 0;
+}
